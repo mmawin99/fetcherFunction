1948ea0bf46225f5766bd60a7e95cd562bfba229
Navin Sae-heng <41543586+mmawin99@users.noreply.github.com>
2025-06-30 21:05:16 +0700

    fix: update POST request with enhanced headers

--- netlify/functions/nexus.js
+++ netlify/functions/nexus.js
@@ -44,7 +44,24 @@ exports.handler = async (event, context) => {
         }
     } else if (event.httpMethod === 'POST') {
         try {
-            const response = await axios.get('https://monomax.me');
+            const response = await axios.get('https://www.monomax.me/', {
+                headers: {
+                    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                    'accept-language': 'en,th;q=0.9,en-US;q=0.8',
+                    'cache-control': 'no-cache',
+                    'pragma': 'no-cache',
+                    'priority': 'u=0, i',
+                    'sec-ch-ua': '"Not)A;Brand";v="8", "Chromium";v="138", "Microsoft Edge";v="138"',
+                    'sec-ch-ua-mobile': '?0',
+                    'sec-ch-ua-platform': '"Windows"',
+                    'sec-fetch-dest': 'document',
+                    'sec-fetch-mode': 'navigate',
+                    'sec-fetch-site': 'none',
+                    'sec-fetch-user': '?1',
+                    'upgrade-insecure-requests': '1',
+                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36 Edg/138.0.0.0'
+                }
+            });
             const html = response.data;
             return {
                 statusCode: 200,
